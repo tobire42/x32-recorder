f2209718c3e908739c6e3efe282a28f9b8b74a99
Tobias Reineke <tobi@g3th.net>
2022-10-25 21:18:46 +0200

    Add method to recording class

--- x32recorder/recorder/models.py
+++ x32recorder/recorder/models.py
@@ -16,5 +16,13 @@ class Recording(models.Model):
     duration = models.DurationField(default=None, blank=True, null=True)
     state = models.IntegerField(default=NEW)
 
+    @classmethod
+    def get_active():
+        active_recordings = Recording.objects.exclude(state=Recording.STOPPED)
+        try:
+            return active_recordings.get()
+        except Recording.DoesNotExist:
+            return None
+
     def __str__(self) -> str:
         return f"Recorded on {self.date} - {self.duration}"
