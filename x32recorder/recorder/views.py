f033bd7eb22f5b18e42d892311d3f74cb2d7aab8
Tobias Reineke <tobi@g3th.net>
2022-10-25 20:40:41 +0200

    Formatting

--- x32recorder/recorder/views.py
+++ x32recorder/recorder/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 
 from .models import Recording
 
+
 def index(request):
-    return render(request, "index.html", {
-        "name": "Marten",
-        "recordings": Recording.objects.all()
-    })
+    return render(
+        request, "index.html", {"name": "Marten", "recordings": Recording.objects.all()}
+    )
+
 
 def startrecording(request):
     rec = Recording(channel_count=2, filename="new.wav")
