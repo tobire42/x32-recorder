60a7e62cbe1d9ebeb1d6805690ba80437df0e796
Tobias Reineke <tobi@g3th.net>
2022-11-15 21:54:33 +0100

    Add templates to admin

--- x32recorder/recorder/admin.py
+++ x32recorder/recorder/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Recording
+from .models import Recording, RecordingTemplate, RecordingTemplateChannel
 
 admin.site.register(Recording)
+admin.site.register(RecordingTemplate)
+admin.site.register(RecordingTemplateChannel)
