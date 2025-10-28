347e0c6c1717bf63849528079c8a6ed850e09151
Tobias Reineke <tobi@g3th.net>
2025-10-28 20:42:58 +0100

    Remove non-functioning start and stop recording views, use ModelView Actions instead

--- x32recorder/recorder/views.py
+++ x32recorder/recorder/views.py
@@ -18,16 +18,3 @@ def index(request):
         },
     )
 
-
-def startrecording(request):
-    filename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".wav"
-    rec = Recording(channels=[1, 2], filename=filename)  # Default to channels 1 and 2
-    rec.save()
-    return redirect(index)
-
-
-def stoprecording(request):
-    recording_active = Recording.get_active()
-    recording_active.state = Recording.STOP
-    recording_active.save()
-    return redirect(index)
