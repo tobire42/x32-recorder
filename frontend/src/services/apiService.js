fe984a7e2efa20f49852f8db1acdcdd6f29c0ffc
Tobias Reineke <tobi@g3th.net>
2025-11-11 22:10:19 +0100

    We can just ust a relativ url since vite dev server is already proxying api endpoints

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Configure the base URL for your Django API
-const API_BASE_URL = 'http://localhost:8000/api'
+const API_BASE_URL = '/api'
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
